96df70677cadcb701c2c71f3b798fe971392a4b1
Anna shabbagh <afreenshabbagh@gmail.com>
2024-12-27 19:25:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,123 +1,79 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import UseMemo from "./useMemo";
+import ReactMemo from "./reactMemo";
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [customTask, setCustomTask] = useState("");
 
-const MENU_DATA = [
-  {
-    id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
-    price: 15.99,
-    img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-  },
-  {
-    id: 2,
-    title: "diner double",
-    category: "lunch",
-    price: 13.99,
-    img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-  },
-  {
-    id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
-    price: 6.99,
-    img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-  },
-  {
-    id: 4,
-    title: "country delight",
-    category: "breakfast",
-    price: 20.99,
-    img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-  },
-  {
-    id: 5,
-    title: "egg attack",
-    category: "lunch",
-    price: 22.99,
-    img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-  },
-  {
-    id: 6,
-    title: "oreo dream",
-    category: "shakes",
-    price: 18.99,
-    img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-  },
-  {
-    id: 7,
-    title: "bacon overflow",
-    category: "breakfast",
-    price: 8.99,
-    img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
-  },
-  {
-    id: 8,
-    title: "american classic",
-    category: "lunch",
-    price: 12.99,
-    img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
-  },
-  {
-    id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
-    price: 16.99,
-    img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
-];
+  const handleAddTodo = () => {
+    setTasks([...tasks, "New todo"]);
+  };
 
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
 
-const Menu = () => {
-  const [list, setlist] = useState(MENU_DATA);
+  const handleCustomTaskChange = (e) => {
+    setCustomTask(e.target.value);
+  };
 
-  const onFilter = (category) => {
-    if (category === "all") {
-      setlist(MENU_DATA);
+  const handleCustomTaskSubmit = () => {
+    if (customTask.length > 5) {
+      setTasks([...tasks, customTask]);
+      setCustomTask("");
     } else {
-      const matches = MENU_DATA.filter((i) => i.category === category);
-
-      setlist(matches);
+      alert("Task must be more than 5 characters long.");
     }
   };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div id="main">
-      <h1>Our Menu</h1>
+      <button id="add-todo-btn" onClick={handleAddTodo}>
+        Add todo
+      </button>
 
-      <div>
-        <button  id="filter-btn-0" onClick={() => onFilter("all")}>All</button>
-        <button data-test-id="menu-item-breakfast" id="filter-btn-1" onClick={() => onFilter("breakfast")}>Breakfast</button>
-        <button data-test-id="menu-item-lunch" id="filter-btn-2" onClick={() => onFilter("lunch")}>Lunch</button>
-        <button data-test-id="menu-item-shakes" id="filter-btn-3" onClick={() => onFilter("shakes")}>Shakes</button>
-      </div>
-
-      <div>
-        {list.map((i) => (
-          <div key={i.id}>
-            <div>
-              <img src={i.img} alt={i.title} />
-            </div>
-            <div>
-              <div>
-                <span>{i.title}</span>
-                <span>{i.price}</span>
-              </div>
-              <div>{i.desc}</div>
-            </div>
-          </div>
+      <button id="increment-btn" onClick={handleIncrement}>
+        {counter}
+      </button>
+      <input
+        id="skill-input"
+        type="text"
+        value={customTask}
+        onChange={handleCustomTaskChange}
+        placeholder="Enter custom task"
+      />
+      <button onClick={handleCustomTaskSubmit} id="skill-btn">
+        Add Skill
+      </button>
+      <div id="item-jumbotron">
+        {tasks.map((task, index) => (
+          <div key={index}>{task}</div>
         ))}
       </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button
+              id={`todo-${index}`}
+              onClick={() => handleRemoveTask(index)}
+            >
+              New Todo
+            </button>
+          </li>
+        ))}
+      </ul>
+      <UseMemo />
+      <ReactMemo />
     </div>
   );
 };
 
-export default Menu;
+export default App;
